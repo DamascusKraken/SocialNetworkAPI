926ced3f5ba37f59eb6547a1d37ac579d93610ff
DamascusKraken <nick.canchola.7.27@gmail.com>
2024-01-08 22:57:42 -0600

    Models user/thougts. model index

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+//Schema to create User model
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
+  thoughts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+//Creates a virtual property for friendCount
+userSchema.virtual('friendaCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
