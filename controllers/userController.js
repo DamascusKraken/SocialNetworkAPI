ab9ec9fe29e173fdccf91372f2f4f668bad55812
DamascusKraken <nick.canchola.7.27@gmail.com>
2024-01-09 01:31:54 -0600

    Completed friends add/delete routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -41,6 +41,7 @@ module.exports = {
     }
   },
 
+  // /api/users
   //create a new user
   async createUser(req, res) {
     try {
@@ -60,7 +61,7 @@ module.exports = {
       const { username, email } = req.body;
       const updatedUser = await User.findOneAndUpdate(
         { _id: req.params.userId },
-        { $set: {username: username, email: email} },
+        { $set: { username: username, email: email } },
         { new: true }
       );
 
@@ -76,21 +77,81 @@ module.exports = {
 
   //Delete to remove user by _id
   async deleteUser(req, res) {
-    try{
-        const deletedUser = await User.findOneAndDelete({_id: req.params.userId});
+    try {
+      const deletedUser = await User.findOneAndDelete({
+        _id: req.params.userId,
+      });
 
-        if(!deletedUser) {
-            return res.status(404).json({ error: "User not Found " });
-        }
-        //Delete thoughts associated with user when deleted. 
-        await Thought.deleteMany({ username: deletedUser.username });
+      if (!deletedUser) {
+        return res.status(404).json({ error: "User not Found " });
+      }
+      //Delete thoughts associated with user when deleted.
+      await Thought.deleteMany({ username: deletedUser.username });
 
-        res.json({mesage: "User and thought deleted succesfully."})
+      res.json({ mesage: "User and thought deleted succesfully." });
     } catch (err) {
-        res.status(500).json({ error: "Interal Server Error" });
+      res.status(500).json({ error: "Interal Server Error" });
     }
   },
 
-  
+  // /api/users/:userId/friends/:friendId
+  // Post to add a friend to the user's friend list
+  async addFriend(req, res) {
+    try {
+      const friendId = req.params.friendId;
 
+      //validate user and friend exist
+      const user = await User.findOne({ _id: req.params.userId });
+      const friend = await User.findOne(friendId);
+
+      if (!user || !friend) {
+        return res.status(404).json({ error: "User or friend not found" });
+      }
+
+      //check if friend is already on user's friends list
+      if (user.friends.includes(friendId)) {
+        return res
+          .status(400)
+          .json({ error: "Friend already on user's friends list." });
+      }
+
+      //Add friend to user's friends list
+      user.friends.push(friendId);
+      await user.save();
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  },
+
+  //Delete friend from user's friends list
+  async deleteFriend(req, res) {
+    try {
+      const friendId = req.params.friendId;
+
+      //check if user exists
+      const user = await User.findOne({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      //check friend exists in user's friend list
+      const friendIndex = user.friends.indexOf(friendId);
+      if (friendIndex === -1) {
+        return res
+          .status(400)
+          .json({ error: "Friend no in the user's friend list" });
+      }
+
+      //remove the friend from user's friend list
+      user.friends.splice(friendIndex, 1);
+      await user.save();
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ error: "Interal Server Error" });
+    }
+  },
 };
